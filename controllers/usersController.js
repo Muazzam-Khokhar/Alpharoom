ff7795f0e696e815cbe54d3ad24e7e2366c8282d
Muazzam Manzoor <muazzamkhokhar.mk@gmail.com>
2023-06-09 16:26:22 +0500

    4th commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,7 +7,7 @@ const saltRound = 10;
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
-    
+
     const userExists = await User.findOne({ email })
     if (userExists) {
         res.status(400)
